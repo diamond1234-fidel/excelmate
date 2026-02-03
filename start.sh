a784d99bab46f650d4a955c97ae6a125294920e4
Diamondfidel1234 <88415553+diamond1234-fidel@users.noreply.github.com>
2026-02-03 14:57:18 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Install Python runtime (only runs when container starts)
+# Install Python runtime at container start
 node cli/index.js ppman install python=3.11.4
 
 # Start the Piston API
