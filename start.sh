1fe22e174b51ed8854ea0bbc408e5d92e5c2f485
Diamondfidel1234 <88415553+diamond1234-fidel@users.noreply.github.com>
2026-02-03 14:59:59 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-# Install Python runtime at container start
+# Install Python if not installed
 node cli/index.js ppman install python=3.11.4
 
-# Start the Piston API
+# Optional: install packages like Pandas
+node cli/index.js ppman install python=3.11.4 pip
+python3 -m pip install pandas openpyxl
+
+# Start the API
 node index.js
